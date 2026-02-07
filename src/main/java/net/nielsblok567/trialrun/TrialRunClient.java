bbe825f59307edc835e8aba1c7ca1c9a304a99e7
niels567 <nielsbloklauridsen@gmail.com>
2026-02-07 20:07:03 +0100

    Initial commit

---
+++ src/main/java/net/nielsblok567/trialrun/TrialRunClient.java
@@ -0,0 +1,10 @@
+package net.nielsblok567.trialrun;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrialRunClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/nielsblok567/trialrun/TrialRunDataGenerator.java
@@ -0,0 +1,11 @@
+package net.nielsblok567.trialrun;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TrialRunDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
